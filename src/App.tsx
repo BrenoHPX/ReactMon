416f37166b538d9db1c68131a6f5634934310a73
Breno Xavier <101307043+BrenoHPX@users.noreply.github.com>
2022-11-04 19:34:21 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyle from './config/GlobalStyle'
+import Routes from './routes/Routes'
+
+function App(): JSX.Element {
+	return (
+		<>
+			<GlobalStyle />
+			<Routes />
+		</>
+	)
+}
+
+export default App
---
+++ src/config/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, #root {
+        min-height: 100vh;
+    }
+`
+export default GlobalStyle
